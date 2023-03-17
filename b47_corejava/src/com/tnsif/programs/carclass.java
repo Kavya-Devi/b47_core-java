2cba9c483b1574aed993e91ccf3196deb14c26ab
praveen kavya <praveen kavya@LAPTOP-SG2BT6M5>
2023-03-17 14:18:18 +0530

    first commit

--- b47_corejava/src/com/tnsif/programs/carclass.java
+++ b47_corejava/src/com/tnsif/programs/carclass.java
@@ -1,10 +1,26 @@
 package com.tnsif.programs;
 
+//program to demonstrate class and object
+
 public class carclass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	int model;
+	int reg;
+	//member functions for the class
+	void forward() {
+		System.out.println("car is in forward direction");
 	}
-
+	void backward() {
+		System.out.println("car is in backward direction");
+		
+	}
+	void stop()
+	{
+		System.out.println("car is not moving");
+	}
 }
+
+
+
+	
+
+
